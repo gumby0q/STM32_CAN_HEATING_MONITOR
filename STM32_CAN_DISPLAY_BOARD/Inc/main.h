a0fb4634bc5ae41e7ecc1cec111cf0e06f4289b6
anatolii <gumbygm0@gmail.com>
2024-03-06 12:05:02 +0200

    names and folders refactoring

---
+++ STM32_CAN_DISPLAY_BOARD/Inc/main.h
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define BUTTON_0_Pin GPIO_PIN_0
+#define BUTTON_0_GPIO_Port GPIOA
+#define BUTTON_1_Pin GPIO_PIN_1
+#define BUTTON_1_GPIO_Port GPIOA
+#define BUTTON_2_Pin GPIO_PIN_2
+#define BUTTON_2_GPIO_Port GPIOA
+#define BUTTON_3_Pin GPIO_PIN_3
+#define BUTTON_3_GPIO_Port GPIOA
+#define DISPLAY_D0_Pin GPIO_PIN_0
+#define DISPLAY_D0_GPIO_Port GPIOB
+#define DISPLAY_D1_Pin GPIO_PIN_1
+#define DISPLAY_D1_GPIO_Port GPIOB
+#define DISPLAY_D2_Pin GPIO_PIN_2
+#define DISPLAY_D2_GPIO_Port GPIOB
+#define BACKLIGHT_Pin GPIO_PIN_12
+#define BACKLIGHT_GPIO_Port GPIOB
+#define DISPLAY_A0_Pin GPIO_PIN_13
+#define DISPLAY_A0_GPIO_Port GPIOB
+#define DISPLAY_RESET_Pin GPIO_PIN_14
+#define DISPLAY_RESET_GPIO_Port GPIOB
+#define DISPLAY_CS_Pin GPIO_PIN_15
+#define DISPLAY_CS_GPIO_Port GPIOB
+#define CAN_ENABLE_Pin GPIO_PIN_8
+#define CAN_ENABLE_GPIO_Port GPIOA
+#define DISPLAY_D3_Pin GPIO_PIN_3
+#define DISPLAY_D3_GPIO_Port GPIOB
+#define DISPLAY_D4_Pin GPIO_PIN_4
+#define DISPLAY_D4_GPIO_Port GPIOB
+#define DISPLAY_D5_Pin GPIO_PIN_5
+#define DISPLAY_D5_GPIO_Port GPIOB
+#define DISPLAY_D6_Pin GPIO_PIN_6
+#define DISPLAY_D6_GPIO_Port GPIOB
+#define DISPLAY_D7_Pin GPIO_PIN_7
+#define DISPLAY_D7_GPIO_Port GPIOB
+#define DISPLAY_RD_Pin GPIO_PIN_8
+#define DISPLAY_RD_GPIO_Port GPIOB
+#define DISPLAY_WR_Pin GPIO_PIN_9
+#define DISPLAY_WR_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
